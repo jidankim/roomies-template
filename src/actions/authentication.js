6658c8bc797f2e9f82fed15089ffa1fe355ebb5b
Dan Kim <jidankim@gmail.com>
2018-06-03 22:15:41 +0900

    follow tutorial step15 check status and logout

--- src/actions/authentication.js
+++ src/actions/authentication.js
@@ -5,7 +5,11 @@ import {
     AUTH_LOGIN_FAILURE,
     AUTH_REGISTER,
     AUTH_REGISTER_SUCCESS,
-    AUTH_REGISTER_FAILURE
+    AUTH_REGISTER_FAILURE,
+    AUTH_GET_STATUS,
+    AUTH_GET_STATUS_SUCCESS,
+    AUTH_GET_STATUS_FAILURE,
+    AUTH_LOGOUT
 } from './ActionTypes';
 
 /*=======================
@@ -80,5 +84,55 @@ export function registerFailure(error) {
     return {
         type: AUTH_REGISTER_FAILURE,
         error
+    };
+}
+
+/* GET STATUS */
+export function getStatusRequest() {
+    return (dispatch) => {
+        // Inform Get Status API is starting
+        dispatch(getStatus());
+
+        return axios.get('/api/account/getInfo')
+        .then((response) => {
+            dispatch(getStatusSuccess(response.data.info.username));
+        }).catch((error) => {
+            dispatch(getStatusFailure());
+        });
+    };
+}
+
+export function getStatus() {
+    return {
+        type: AUTH_GET_STATUS
+    };
+}
+
+export function getStatusSuccess(username) {
+    return {
+        type: AUTH_GET_STATUS_SUCCESS,
+        username
+    };
+}
+
+export function getStatusFailure() {
+    return {
+        type: AUTH_GET_STATUS_FAILURE
+    };
+}
+
+/* Logout */
+export function logoutRequest() {
+    return (dispatch) => {
+        return axios.post('/api/account/logout')
+        .then((response) => {
+            dispatch(logout());
+        });
+    };
+}
+
+export function logout() {
+    return {
+        type: AUTH_LOGOUT
     };
 }
