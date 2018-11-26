1c05a61ecaa09bb3c4cf43263cffaf42f792fd90
Dan Kim <jidankim@gmail.com>
2018-11-26 17:09:31 +0900

    mongodb to mysql part 1

--- server/main.js
+++ server/main.js
@@ -2,23 +2,31 @@ import express from 'express';
 import path from 'path';
 import morgan from 'morgan'; // HTTP REQUEST LOGGER
 import bodyParser from 'body-parser'; // PARSE HTML BODY
-import mongoose from 'mongoose';
 import session from 'express-session';
+import mysql from 'mysql';
 import WebpackDevServer from 'webpack-dev-server';
 import webpack from 'webpack';
 
 const app = express();
-const port = 3000;
-const devPort = 4000;
+const port = 3306;
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-const db = mongoose.connection;
-db.on('error', console.error);
-db.once('open', () => { console.log('Connected to mongodb server'); });
+// const db = mongoose.connection;
+// db.on('error', console.error);
+// db.once('open', () => { console.log('Connected to mongodb server'); });
 // mongoose.connect('mongodb://username:password@host:port/database=');
-mongoose.connect('mongodb://localhost/codelab');
+// mongoose.connect('mongodb://localhost/codelab');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: port,
+    user: 'tester',
+    password: '1234',
+    database: 'cs360_tutorial'
+});
+connection.connect();
 
 /* use session */
 app.use(session({
@@ -53,8 +61,8 @@ if (process.env.NODE_ENV == 'development') {
     const compiler = webpack(config);
     const devServer = new WebpackDevServer(compiler, config.devServer);
     devServer.listen(
-        devPort, () => {
-            console.log('webpack-dev-server is listening on port', devPort);
+        port, () => {
+            console.log('webpack-dev-server is listening on port', port);
         }
     );
 };
