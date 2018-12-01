7bcad8234e5623f8e06b86ebb4171dacdfd86f25
Dan Kim <jidankim@gmail.com>
2018-12-01 22:11:14 +0900

    update package json file

--- DB_Constructor_Query.sql
+++ DB_Constructor_Query.sql
@@ -1,11 +1,11 @@
 create database if not exists ROOMIE;
 use ROOMIE;
 
--- drop table COMMENTS;
--- drop table STUDENT;
--- drop table ROOM;
--- drop table PREFERENCE;
--- drop table DORMITORY;
+drop table COMMENTS;
+drop table STUDENT;
+drop table ROOM;
+drop table PREFERENCE;
+drop table DORMITORY;
 
 create table if not exists PREFERENCE (
 	Student_ID int,
@@ -40,6 +40,7 @@ create table if not exists ROOM (
 
 create table if not exists STUDENT (
 	Student_ID int,
+    Password varchar(255) not null,
     First_Name varchar(255) not null,
     Last_Name varchar(255) not null,
     Age int,
@@ -63,27 +64,27 @@ create table if not exists COMMENTS (
     foreign key (Room_ID) references ROOM(Room_ID)
 );
 
-#Inserting dummy data for DORMITORY
+-- Inserting dummy data for DORMITORY
 insert into DORMITORY (Dorm_ID, Dorm_Name, Location, Fee, Total_Capacity) values 
 	("N20", "성실관", "North", 420000, 400)
     ;
 
-#Inserting dummy data for ROOM
+-- Inserting dummy data for ROOM
 insert into ROOM (Room_ID, Dorm_ID, Floor, Capacity) values
 	("N20_228", "N20", 2, 2)
     ;
 
-#Inserting dummy data for STUDENT
-insert into STUDENT (Student_ID, First_Name, Last_Name, Age, Major, Club, Phone_Number, Room_ID) values 
-	(20130736, "Andrew", "Kim", 24, "Computer Science", "PASSION", "000-1234-5678", "N20_228")
+-- Inserting dummy data for STUDENT
+insert into STUDENT (Student_ID, Password, First_Name, Last_Name, Age, Major, Club, Phone_Number, Room_ID) values 
+	(20130736, "password", "Andrew", "Kim", 24, "Computer Science", "PASSION", "000-1234-5678", "N20_228")
     ;
 
-#Inserting dummy data for PREFERENCE
+-- Inserting dummy data for PREFERENCE
 insert into PREFERENCE (Student_ID, Smokes, Sleep_Start_Time, Sleep_End_Time, Music_Preference, Hobby) values
 	(20130736, False, "22:00:00", "06:00:00", "Orchestra", "Game")
     ;
 
-#Inserting dummy data for COMMENTS
+-- Inserting dummy data for COMMENTS
 insert into COMMENTS (Student_ID, Room_ID, Content) values 
 	(20130736, "N20_228", "This room sucks man")
     ;
