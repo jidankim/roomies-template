6fca160ab407995c45940deafb2cd924db8f8482
김대원 <andrewpk95@hotmail.com>
2018-12-06 22:14:49 +0900

    Pre-writing get/set student profile and preference

--- server/routes/__prewriting.js
+++ server/routes/__prewriting.js
@@ -6,42 +6,138 @@ import pool from '../config.js';
 
 const router = express.Router();
 
-router.post('/getpreference', (req, res) => {
-    var id = parseInt(req.body.studentID);
+//Get Student Information, given Student ID
+router.post('/getprofile', (req, res) => {
+    //Extracting values from the request
+    var student_id = parseInt(req.body.student_id);
 
     pool.getConnection((err, connection) => {
-        let queryString = "SELECT * FROM preference WHERE student_id = ?"
-        connection.query(queryString, [id], (err, results, fields) => {
-            if (err) throw err;
+        let queryString = "SELECT * FROM student WHERE student_id = ?"
+        connection.query(queryString, [student_id], (err, results, fields) => {
+            if (err) {
+                connection.release();
+                console.log(err);
+                throw err;
+            }
             //If no result, return null
             if (results.length == 0) {
-
+                connection.release();
+                return res.json({exists: false, result: null});
             }
+            //Return result of query
             else {
-                return res.json({result: result[0]});
+                connection.release();
+                return res.json({exists: true, result: result[0]});
             }
         });
     });
 })
 
-router.post('/savepreference', (req, res) => {
-	var id = parseInt(req.body.studentID);
+//Set Student Information, given Student ID and student data (First Name, Last Name, Age, Major, PhoneNumber)
+router.post('/updateprofile', (req, res) => {
+    //Extracting variables from the request
+    var student_id = parseInt(req.body.student_id);
+    var first_name = req.body.first_name;
+    var last_name = req.body.last_name;
+    var age = req.body.age;
+    var major = req.body.major;
+    var phonennumber = req.body.phonennumber;
+
+    pool.getConnection((err, connection) => {
+        let queryString = "UPDATE student SET first_name = ?, last_name = ?, age = ?, major = ?, phonenumber = ? WHERE student_id = ?"
+        connection.query(queryString, [first_name, last_name, age, major, phonenumber, student_id], (err, results, fields) => {
+            if (err) {
+                connection.release();
+                console.log(err);
+                throw err;
+            }
+            connection.release();
+            return res.json({success = true});
+        });
+    });
+});
+
+//Get Preference, given Student ID
+router.post('/getpreference', (req, res) => {
+    //Extracting values from the request
+    var student_id = parseInt(req.body.student_id);
+
+    pool.getConnection((err, connection) => {
+        let queryString = "SELECT * FROM preference WHERE student_id = ?"
+        connection.query(queryString, [student_id], (err, results, fields) => {
+            if (err) {
+                connection.release();
+                console.log(err);
+                throw err;
+            }
+            //If no result, return null
+            if (results.length == 0) {
+                connection.release();
+                return res.json({exists: false, result: null});
+            }
+            //Return result of query
+            else {
+                connection.release();
+                return res.json({exists: true, result: result[0]});
+            }
+        });
+    });
+})
+
+//Set Preference, given Student ID and preference data
+router.post('/updatepreference', (req, res) => {
+	//Extracting variables from the request
+    var student_id = parseInt(req.body.student_id);
+    var smoker = req.body.smoker;
+    var sleep_start_time = req.body.sleep_start_time;
+    var sleep_end_time = req.body.sleep_end_time;
+    var music_Preference = req.body.music_Preference;
+    var hobby = req.body.hobby;
+    var club = req.body.club;
 
 	pool.getConnection((err, connection) => {
 		let queryString = "SELECT * FROM preference WHERE student_id = ?"
-		connection.query(queryString, [id], (err, results, fields) => {
-			if (err) throw err;
+		connection.query(queryString, [student_id], (err, results, fields) => {
+			if (err) {
+                connection.release();
+                console.log(err);
+                throw err;
+            }
 			//First time entering preference - save preference using INSERT
 			if (results.length == 0) {
-
+                queryString = "INSERT INTO preference VALUES ?";
+                connection.query(queryString, [[student_id, smoker, sleep_start_time, sleep_end_time, music_Preference, hobby, club]], (err, result) => {
+                    if (err) {
+                        connection.release();
+                        console.log(err);
+                        throw err;
+                    }
+                    connection.release();
+                    return res.json({success: true});
+                });
 			}
             //Editing preference - save preference using UPDATE
 			else if (results.length == 1) {
-
+                queryString = "UPDATE preference SET smoker = ?, sleep_start_time = ?, sleep_end_time = ?, music_Preference = ?, hobby = ?, club = ? where student_id = ?";
+                connection.query(queryString, [smoker, sleep_start_time, sleep_end_time, music_Preference, hobby, club, student_id], (err, result) => {
+                    if (err) {
+                        connection.release();
+                        console.log(err);
+                        throw err;
+                    }
+                    connection.release();
+                    return res.json({success: true});
+                });
 			}
-			else {
-
-			}
+            //Error
+            else {
+                connection.release();
+                return res.status(500).json({
+                    error: "error in DB: two preferences exist for the student_id: " + student_id,
+                    code: 999
+                });
+            }
+			
 		});
 	});
 });
